f407d5b48ed7aaa5d55166ac21f15af7c3a31cc4
ludoph <phounphonh.ludovic@gmail.com>
2023-10-03 16:57:22 +0200

    feat: set sql request

--- postgresql-dump/queries_sql.sql
+++ postgresql-dump/queries_sql.sql
@@ -9,18 +9,18 @@ WHERE date_naissance <= DATE_SUB(CURDATE(), INTERVAL 50 YEAR)
 ORDER BY nom, prenom;
 
 -- La liste des acteurs/actrices principaux pour un film donné (en supposant que l'acteur principal est celui avec le rôle "Acteur Principal")
-SELECT acteurs.nom, acteurs.prenom
+SELECT acteurs.nom, acteurs.prenom, films.titre
 FROM acteurs
 INNER JOIN jouer ON acteurs.id_acteur = jouer.id_acteur
 INNER JOIN films ON jouer.id_film = films.id_film
-INNER JOIN roles ON jouer.id_role = roles.id_role
-WHERE films.titre = 'Titre du film' AND roles.nom = 'Acteur Principal';
+WHERE jouer.role = 'Principal'
+AND film.titre = ['Titre film']
 
 -- La liste des films pour un acteur/actrice donné (en supposant que vous connaissez l'ID de l'acteur/actrice)
 SELECT films.titre, films.annee
 FROM films
 INNER JOIN jouer ON films.id_film = jouer.id_film
-WHERE jouer.id_acteur = ID_de_l_acteur;
+WHERE jouer.id_acteur = [ID_de_l_acteur];
 
 -- Ajouter un acteur/actrice (assumant que vous avez les valeurs pour nom, prenom et date de naissance)
 INSERT INTO acteurs (nom, prenom, date_naissance)
@@ -28,15 +28,16 @@ VALUES ('Nouveau', 'Acteur', '1990-01-01');
 
 -- Modifier un film (assumant que vous avez l'ID du film que vous souhaitez modifier et la nouvelle valeur pour le titre) 
 UPDATE films
-SET titre = 'Nouveau Titre'
-WHERE id_film = ID_du_film;
+SET titre = ['Nouveau Titre']
+WHERE id_film = [ID_du_film];
 
 -- Supprimer un acteur/actrice (assumant que vous avez l'ID de l'acteur/actrice que vous souhaitez supprimer
 DELETE FROM acteurs
-WHERE id_acteur = ID_de_l_acteur;
+WHERE id_acteur = [ID_de_l_acteur];
 
 -- Afficher les 3 derniers acteurs/actrices ajouté(e)s
 SELECT nom, prenom, date_naissance
 FROM acteurs
 ORDER BY id_acteur DESC
 LIMIT 3;
+
