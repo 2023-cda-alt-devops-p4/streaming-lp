4d019085d2b49382f69ff64537c5b42882efc034
ludoph <phounphonh.ludovic@gmail.com>
2023-10-04 15:30:50 +0200

    fix: update trigger

--- mysql-dump/trigger.sql
+++ mysql-dump/trigger.sql
@@ -9,14 +9,26 @@ BEGIN
 END //
 DELIMITER ;
 
-
 -- Déclencheur (Trigger) pour garder une trace des modifications des utilisateurs :
 DELIMITER //
 CREATE TRIGGER ArchiveModificationsUtilisateurs
 AFTER UPDATE ON utilisateurs
 FOR EACH ROW
 BEGIN
-    INSERT INTO archives (date_modif, ancienne_val, nouvelle_val)
-    VALUES (NOW(), OLD.nom, NEW.nom);
+
+    IF OLD.nom <> NEW.nom THEN
+        INSERT INTO archives (date_modif, ancienne_val, nouvelle_val, nom_col)
+        VALUES ('nom', NOW(), OLD.nom, NEW.nom, nom_col);
+    ELSEIF OLD.prenom <> NEW.prenom THEN
+        INSERT INTO archives (date_modif, ancienne_val, nouvelle_val, nom_col)
+        VALUES ('prenom', NOW(), OLD.prenom, NEW.prenom, nom_col);
+    ELSEIF OLD.mail <> NEW.mail THEN
+        INSERT INTO archives (date_modif, ancienne_val, nouvelle_val, nom_col)
+        VALUES ('mail', NOW(), OLD.mail, NEW.mail, nom_col);
+    ELSEIF OLD.mdp <> NEW.mdp THEN
+        INSERT INTO archives (date_modif, ancienne_val, nouvelle_val, nom_col)
+        VALUES ('mdp', NOW(), OLD.mdp, NEW.mdp, nom_col);
+    END IF;
+
 END //
 DELIMITER ;
